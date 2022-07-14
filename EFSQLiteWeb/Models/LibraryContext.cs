b75438ca4d8c7d4b7aeeeb3fd19dbf8ecc7d27b4
jjvera96 <dev.jjvera96@gmail.com>
2022-07-14 11:00:35 -0500

    Add project files.

---
+++ EFSQLiteWeb/Models/LibraryContext.cs
@@ -0,0 +1,16 @@
+﻿using EFSQLiteWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFSQLiteWeb.Models
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; } 
+
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ EFSQLiteWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using EFSQLiteWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EFSQLiteWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly LibraryContext _context;
+        public List<Author> authorList { get; set; } = new ();
+        public List<Book> bookList { get; set; } = new();
+
+        public IndexModel(ILogger<IndexModel> logger, LibraryContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            authorList =  _context.Authors.ToList();
+            bookList =  _context.Books.ToList();
+        }
+    }
+}
